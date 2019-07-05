5e8b7b10366beac688142c4580de8d3596a9fdbe
Antons Kranga <anton@agilestacks.com>
2019-07-06 00:23:23 +0300

    Fixing various small discrepencies caused by libraries and variables changes

--- nbextensions/utils.py
+++ nbextensions/utils.py
@@ -1,6 +1,5 @@
 from kubernetes import config
-import hashlib
-import os.path
+import hashlib, requests, os
 
 def sha1(*argv):
     """returns sha1 encoded string. optionally supports salt
@@ -8,7 +7,7 @@ def sha1(*argv):
     s = ':'.join(argv)
     return hashlib.md5(s.encode()).hexdigest()
 
-def md5sum(filename):
+def get_md5(fname):
     """Returns md5 sum"""
     hash_md5 = hashlib.md5()
     with open(fname, "rb") as f:
@@ -21,14 +20,19 @@ def download_file(url, download_to, md5sum):
     # NOTE the stream=True parameter below
 
     if os.path.isfile(download_to) and md5sum:
-        if md5sum == md5sum(download_to):
+        if md5sum == get_md5(download_to):
             print(f"It seems {download_to} has been already downloaded")
             print("Moving on...")
             return
+    
 
     print('Downloading...')
     with requests.get(url, stream=True) as r:
         r.raise_for_status()
+        fdir = os.path.dirname(download_to)
+        if not os.path.isdir(fdir):
+            os.makedirs(fdir, exist_ok=True)
+            
         with open(download_to, 'wb+') as f:
             for chunk in r.iter_content(chunk_size=8192):
                 if chunk: # filter out keep-alive new chunks
