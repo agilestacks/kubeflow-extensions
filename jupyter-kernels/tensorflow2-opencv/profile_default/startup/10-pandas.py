92b7575c21af00dcb2a42c001c802ebed8d128dd
Antons Kranga <anton@agilestacks.com>
2020-11-10 21:19:59 +0200

    Tensorflow2 kernel

---
+++ jupyter-kernels/tensorflow2-opencv/profile_default/startup/10-pandas.py
@@ -0,0 +1,5 @@
+import pandas as pd
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
+pd.set_option('display.expand_frame_repr', False)
